e9995670a921fb0f8336bd3f12abe11c87f3c27b
Parasbuda <parasbuda1234@gmail.com>
2020-10-23 19:49:19 +0545

    button and Touchable Opacity included

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,25 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hello World!!! </Text>;
+const HomeScreen = (props) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hello World!!! </Text>
+      <Button
+        onPress={() =>props.navigation.navigate('Component') }
+        title="Go To the Component Screen"
+      />
+      <TouchableOpacity onPress={()=>props.navigation.navigate('List')}>
+        <Text>Go to List </Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
-  }
+    fontSize: 30,
+  },
 });
 
 export default HomeScreen;
